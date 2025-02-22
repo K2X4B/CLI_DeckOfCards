81898fa9d464ccd6c4372b81cf4906357157f009
Mattchu Pichu <34795746+K2X4B@users.noreply.github.com>
2025-02-22 16:03:43 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,35 +6,3 @@ import Players
 import random
 import time
 
-def deal_one_player():
-    card = (random.choice(deck))
-    print("You were dealt", card.name)
-    #deck.pop(card.id)
-    deck.remove(card)
-    user.hand.append(card)
-
-def deal_two_player():
-    deal_one_player()
-    deal_one_player()
-
-def deal_bj_player_start():
-    deal_two_player()
-
-def hit():
-    deal_one_player()
-
-def show_cards_left():
-    print("There are", len(deck), "cards remaining in the deck")
-
-#--------------------------------[Main Code / Testing]----------------------------------------#
-print(len(deck), " cards remaining in the deck")
-deal_two_player()
-#print(user.hand[0].name)
-print(len(deck), " cards remaining in the deck")
-#deal_one_player()
-deal_bj_player_start()
-print(len(deck), " cards remaining in the deck")
-print("hit")
-hit()
-#print(len(deck), " cards remaining in the deck")
-show_cards_left()
